46705a5f96bfec91f9027f516b6fc777c0c57626
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2022-03-19 11:36:00 +0900

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import Icon from './Icon.js';
+import Icon from './Icon';
 import './App.css';
 
 export default function App() {
--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,7 +1,13 @@
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Icon.css';
 
-export default function Icon({ link, icon }) {
+type IconProps = {
+  link: string;
+  icon: IconDefinition;
+};
+
+export default function Icon({ link, icon }: IconProps) {
   return (
     <a className="Icon" href={link}>
       <FontAwesomeIcon icon={icon} />
